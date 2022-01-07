b7fa5b33fde102ae93a4d821b6d98e6484608ac7
prasad-kumkar <pk111_@live.com>
2022-01-08 02:32:18 +0530

    favicon update

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: 'https://i.ibb.co/DrsCxtM/logo.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
